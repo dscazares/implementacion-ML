a632c8cbd61a37f8e2b7e329656d9ae6a7fe4d69
Daniel Cázares <dscazares@gmail.com>
2022-09-17 19:34:46 -0500

    complete from scratch and analysis

--- Sin Framework/main.py
+++ Sin Framework/main.py
@@ -3,17 +3,25 @@
 import pandas as pd
 from model import LinearRegression, data_split, rmse
 
+# Read CSV
 df = pd.read_csv('Real estate.csv')
 
+# Labels (Y) and features (x) 
 x = df[['X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores', 'X5 latitude', 'X6 longitude']]
 y = df['Y house price of unit area']
 
+# Train test data split
 x_train, y_train, x_test, y_test = data_split(x, y)
 
+# Training Features Escalation
+x_train=(x_train-x_train.mean())/x_train.std()
+
+# Model fit and prediction
 model = LinearRegression()
-
 model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+preds = model.predict(x_test)
 
-error = rmse(y_test, y_pred)
-print("Error", error)
+print("Equation: y = ", model.intercept, end ="")
+for w in model.weights:
+    print(" +", w, "x", end ="")
+print("\nRMSE: ", rmse(y_test, preds))
